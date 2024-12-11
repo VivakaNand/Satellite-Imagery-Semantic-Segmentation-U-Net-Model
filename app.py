652c28565c66cfd3e904ea1da1100ca4feaff07f
Vivaka Nand <44522445+VivakaNand@users.noreply.github.com>
2024-12-11 15:35:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from keras.models import load_model
 
 # Load the model
 @st.cache_resource
-def load_trained_model():
-    return load_model(
-        'satellite-imagery.h5',
-        custom_objects={
-            'dice_loss_plus_1focal_loss': total_loss,
-            'jaccard_coef': jaccard_coef
-        }
-    )
+#def load_trained_model():
+#    return load_model(
+#        'satellite-imagery.h5',
+##        custom_objects={
+ #           'dice_loss_plus_1focal_loss': total_loss,
+ #           'jaccard_coef': jaccard_coef
+ #       }
+ #   )
 
-saved_model = load_trained_model()
+saved_model = load_model('satellite-imagery.h5')
 
 # Define image preprocessing function
 def preprocess_image(image_path, target_size=(256, 256)):
